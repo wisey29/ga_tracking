2f2125a2df03523c441653f1bf255cd0db392d42
yc <yurain2909@gmail.com>
2023-09-29 23:00:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,42 +6,21 @@ function changeBackgroundColor() {
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
 
-    // Track the button click event in Google Analytics
-    gtag('event', 'basic_button_click', {
-        'event_category': 'Color Change',
-        'event_label': 'Background Color Changed'
-    });
-
     // Track the button click event in Google Analytics with additional parameters
-    gtag('event', 'enrich_button_click', {
-    'event_category': 'Enrich Color Change', // Category of the event
-    'event_label': 'Enrich Background Color Changed', // Label for the event
+    gtag('event', 'basic_button_click', {
+    'event_category': 'Color Change', // Category of the event
+    'event_label': 'Background Color Changed', // Label for the event
     'value': 1, // Numeric value associated with the event (optional)
     'currency': 'USD', // Currency for the value (optional)
     'event_action': 'click', // Action associated with the event (optional)
     'event_callback': function() {
         // Callback function to execute after the event is tracked (optional)
-        console.log('Button click event tracked successfully.');
+        console.log('basic_button_click event tracked successfully.');
     },
     'event_transport': 'beacon' // Specify the transport method (optional)
     });
 }
 
-function login() {
-    const username = document.getElementById("username-input").value;    
-    gtag('set', {'user_id': username});
-}
-
-function logout() {
-    gtag('set', {'user_id': null});
-}
-
 // Add an event listener to the button
 const button = document.getElementById("color-button");
 button.addEventListener("click", changeBackgroundColor);
-
-const login_button = document.getElementById("login-button");
-login_button.addEventListener("click", login);
-
-const logout_button = document.getElementById("logout-button");
-logout_button.addEventListener("click", logout);
